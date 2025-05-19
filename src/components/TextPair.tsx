c5bb9e4941db906711fd40bb0300b8c5891b9301
Hakan <hakan@mbp18.com>
2025-05-18 23:33:42 -0500

    Add drag and drop support

--- src/components/TextPair.tsx
+++ src/components/TextPair.tsx
@@ -10,13 +10,13 @@ export const TextPair = (props: TextPairProps) => {
     return <div>
         <label>Original</label>
         <textarea  
-            rows={10} cols={40}  wrap="soft" 
+            rows={20} cols={60}  wrap="soft" 
             onChange = {e => {props.onUpdateText(e.target.value)}} 
             value={props.info.raw}>
         </textarea>
         <label>Converted</label>
         <textarea  
-            rows={10} cols={40}  wrap="soft" value={props.info.converted} onChange= {()=>{}}>
+            rows={20} cols={60}  wrap="soft" value={props.info.converted} onChange= {()=>{}}>
         </textarea>
         <button onClick={props.onDeleteText}>🗑️</button>
     </div>
